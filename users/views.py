7a854e6da521de47daa3e90cef44f20bdc2dded8
Yasin <asinarstanbekov51@gmail.com>
2025-01-06 13:06:16 +0600

    gg

--- users/views.py
+++ users/views.py
@@ -2,27 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
-
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from django.contrib.auth import authenticate
 
 class RegisterView(APIView):
-    def post(self,request):
-        serializer = UserSerializer(data = request.data)
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            return Response({'message':'User created succesfuly'},status =status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_404_BAD_REQUEST)
+            serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class TokenView(APIView):
-    def post(self , request):
+    def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
-        user = authenticate(username = username,password = password)
+        user = authenticate(username=username, password=password)
         if user:
             refresh = RefreshToken.for_user(user)
-            return Response({
-                'refresh':str(refresh),
-                'access':str(refresh,acess_token),
-            })
-        return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-# Create your views here.
+            return Response({'refresh': str(refresh), 'access': str(refresh.access_token)})
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
